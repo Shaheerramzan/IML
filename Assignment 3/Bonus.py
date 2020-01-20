63091d9ed76afaf6750518b1925313bc7b6050f9
shaheer <shaheerramzan1619@gmail.com>
2020-01-20 23:34:20 +0500

    empty files added

--- Assignment 3/Bonus.py
+++ Assignment 3/Bonus.py
@@ -3,17 +3,17 @@ from sklearn import metrics
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from itertools import combinations
+import copy as cpy
+import threading as th
+import cython
 
 
-def work(current_data):
+def main_work(current_data, acc, c):
     for col, col_data in current_data.items():
         if col_data.dtype == object:
             unique_list = col_data.unique()
             for ind in range(0, len(unique_list)):
                 current_data[col] = current_data[col].replace(unique_list[ind], ind)
-
-    # for col in dataHead:
-    #    data[col] = pd.Categorical(data[col])
 
     x_train, x_test, y_train, y_test = train_test_split(current_data, y, test_size=0.85, random_state=1)
 
@@ -21,23 +21,31 @@ def work(current_data):
     gnb.fit(x_train, y_train)
     y_pred_naive = gnb.predict(x_test)
     acc_naive_bayes = metrics.accuracy_score(y_test, y_pred_naive) * 100
-    print("Gaussian Naive Bayes model accuracy(in %):", acc_naive_bayes)
+    if acc[1] < acc_naive_bayes:
+        acc[1] = cpy.deepcopy(acc_naive_bayes)
+        temp = acc[0]
+        del temp
+        acc[0] = list()
+        acc[0] = cpy.deepcopy(c)
 
 
 data = pd.read_csv('student-data.csv')
 y = data['passed']
 data = data.drop(columns='passed')
 dataHead = data.columns
+the_accurate = [list(), 0.0]
+
 new_data = pd.DataFrame()
 
 for n in range(1, 30):
-    print(n)
     comb = combinations(dataHead, n)
     for cmb in comb:
         for item in cmb:
             new_data[item] = data[item]
-        work(new_data)
+        main_work(new_data, the_accurate, cmb)
         del new_data
         new_data = pd.DataFrame()
+        del cmb
+    print(f'combination with choose {n} the minimum till now is: {the_accurate[0]} and accuracy is: {the_accurate[1]}')
     del comb
 
--- Assignment5/LogisticRegressionStudent.py
+++ Assignment5/LogisticRegressionStudent.py
@@ -3,7 +3,7 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression as l_r
+from sklearn.linear_model import LogisticRegression
 
 
 data = pd.read_csv('student-data.csv')
@@ -22,10 +22,10 @@ y = data['passed']
 data = data.drop(columns='passed')
 X_train, X_test, y_train, y_test = train_test_split(data, y, test_size=0.25, random_state=1)
 
-logistic_regression = l_r()
+logistic_regression = LogisticRegression()
 logistic_regression.fit(X_train, y_train)
-y_predict = logistic_regression.predict(X_test)
-acc_logistic_regression = logistic_regression.score(X_test, y_test)*100
-print("Logistic Regression accuracy(in %):", acc_logistic_regression)
+y_predict = logistic_regression.predict(X_test[0:])
+acc_logistic_regression = logistic_regression.score(X_test, y_test)
+print("Logistic Regression accuracy:", acc_logistic_regression)
 cm = metrics.confusion_matrix(y_test, y_predict)
 print(cm)
