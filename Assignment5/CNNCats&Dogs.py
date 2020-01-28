7716aa873b86245d0b7526a5c3beebde3a9cb906
Shaheer Ramzan <38084480+Shaheerramzan@users.noreply.github.com>
2020-01-28 13:06:01 +0500

    takreeban final

--- Assignment5/CNNCats&Dogs.py
+++ Assignment5/CNNCats&Dogs.py
@@ -1,48 +1,130 @@
-# load dogs vs cats dataset, reshape and save to a new file
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import cv2
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Dropout, Activation, Conv2D, MaxPooling2D
+import os
+import re
+import numpy as np
+import pandas as pd
+from keras import backend as K
+from keras import layers, models
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import load_model
 
-# define location of dataset
-folder = '../Datasets/cats&dogs/train/'
-file_list = listdir(folder)
-number_files = len(file_list)
-partition = 5
-batch = int(number_files / partition)
-photos, labels = [list()] * batch, [int] * batch
-# enumerate files in the directory
-for i in range(number_files):
-    # determine class
-    output = 0.0
-    if file_list[i].startswith('cat'):
-        output = 1.0
-    # load image
-    photo = load_img(folder + file_list[i], target_size=(200, 200))
-    temp_photo = photo
-    # convert to numpy array
-    photo = img_to_array(photo)
-    # store
-    photos[i % batch] = photo
-    labels[i % batch] = output
-    del temp_photo
-    del photo
-    del output
-    if i % batch == 0 and i != 0 or i == number_files - 1:
-        # convert to a numpy arrays
-        photos = asarray(photos)
-        labels = asarray(labels)
-        print(photos.shape, labels.shape, i / batch)
-        # save the reshaped photos
-        if i == number_files - 1:
-            st = str(partition)
-        else:
-            st = str(int(i / batch))
-        save('Dataset/dogs_vs_cats_photos' + st + '.npy', photos)
-        save('Dataset/dogs_vs_cats_labels' + st + '.npy', labels)
-        del photos
-        del labels
-        photos, labels = [list()] * batch, [int] * batch
+img_width = 150
+img_height = 150
+TRAIN_DIR = '../Datasets/cats&dogs/train/'
+TEST_DIR = '../Datasets/cats&dogs/test1/'
+train_images_dogs_cats = [TRAIN_DIR + i for i in os.listdir(TRAIN_DIR)]  # use this for full dataset
+test_images_dogs_cats = [TEST_DIR + i for i in os.listdir(TEST_DIR)]
+
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+
+def natural_keys(text):
+    return [atoi(c) for c in re.split('(\d+)', text)]
+
+
+train_images_dogs_cats = train_images_dogs_cats[0:1300] + train_images_dogs_cats[12500:13800]
+
+test_images_dogs_cats.sort(key=natural_keys)
+
+
+def prepare_data(list_of_images):
+    x = []  # images as arrays
+    y = []  # labels
+
+    for image in list_of_images:
+        x.append(cv2.resize(cv2.imread(image), (img_width, img_height), interpolation=cv2.INTER_CUBIC))
+
+    for i in list_of_images:
+        if 'dog' in i:
+            y.append(1)
+        elif 'cat' in i:
+            y.append(0)
+    return x, y
+
+
+X, Y = prepare_data(train_images_dogs_cats)
+print(K.image_data_format())
+
+X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2, random_state=1)
+
+nb_train_samples = len(X_train)
+nb_validation_samples = len(X_val)
+batch_size = 16
+
+model = models.Sequential()
+
+model.add(layers.Conv2D(32, (3, 3), input_shape=(img_width, img_height, 3)))
+model.add(layers.Activation('relu'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+model.add(layers.Conv2D(32, (3, 3)))
+model.add(layers.Activation('relu'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+model.add(layers.Conv2D(64, (3, 3)))
+model.add(layers.Activation('relu'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64))
+model.add(layers.Activation('relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1))
+model.add(layers.Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+model.summary()
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+val_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+train_generator = train_datagen.flow(np.array(X_train), Y_train, batch_size=batch_size)
+validation_generator = val_datagen.flow(np.array(X_val), Y_val, batch_size=batch_size)
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size
+)
+
+model.save_weights('model_wieghts.h5')
+model.save('model_keras.h5')
+
+
+model = load_model('../working/model_keras.h5')
+model.summary()
+
+X_test, Y_test = prepare_data(test_images_dogs_cats)  # Y_test in this case will be []
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+test_generator = val_datagen.flow(np.array(X_test), batch_size=batch_size)
+prediction_probabilities = model.predict_generator(test_generator, verbose=1)
+
+prediction_probabilities = [int(round(p[0])) for p in prediction_probabilities]
+
+counter = range(1, len(test_images_dogs_cats) + 1)
+solution = pd.DataFrame({"id": counter, "label": list(prediction_probabilities)})
+cols = ['label']
+
+for col in cols:
+    solution[col] = solution[col].map(lambda x: str(x).lstrip('[').rstrip(']')).astype(float)
+
+solution.to_csv("dogsVScats.csv", index=False)
